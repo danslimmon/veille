99622f6c8c809e58bd6c515073ac03495677a2d4
Dan Slimmon <dan.slimmon@gmail.com>
2015-07-28 21:43:17 +0000

    Added log version entry to the mix

--- log_entry.go
+++ log_entry.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
+	"strings"
 	"time"
 
 	log "github.com/Sirupsen/logrus"
@@ -37,12 +38,26 @@ func (ent *LogRotationLogEntry) Timestamp() time.Time { return ent.T }
 func (ent *LogRotationLogEntry) Type() string         { return "rotation" }
 func (ent *LogRotationLogEntry) StrVal() string       { return ent.RotationInterval }
 
+// LogVersionLogEntry represents a Nagios log line that specifies the
+// version of the log file. These lines look like:
+//
+//	[1438041600] LOG VERSION: 2.0
+type LogVersionLogEntry struct {
+	T       time.Time
+	Version string
+}
+
+func (ent *LogVersionLogEntry) Timestamp() time.Time { return ent.T }
+func (ent *LogVersionLogEntry) Type() string         { return "version" }
+func (ent *LogVersionLogEntry) StrVal() string       { return ent.Version }
+
 // ParseLogLine takes a line from the Nagios log and returns a
 // LogEntry.
 func ParseLogLine(logLine string) (LogEntry, error) {
 	var err error
 	var re *regexp.Regexp
 	var groups []string
+	var remainder, beforeColon string
 	var timestampInt64 int64
 	var timestamp time.Time
 
@@ -57,7 +72,16 @@ func ParseLogLine(logLine string) (LogEntry, error) {
 		return nil, err
 	}
 	timestamp = time.Unix(timestampInt64, 0)
-	return ParseLogRotationLogEntry(timestamp, groups[2])
+
+	remainder = groups[2]
+	beforeColon = remainder[:strings.IndexByte(remainder, ':')]
+	switch beforeColon {
+	case "LOG ROTATION":
+		return ParseLogRotationLogEntry(timestamp, remainder)
+	case "LOG VERSION":
+		return ParseLogVersionLogEntry(timestamp, remainder)
+	}
+	return nil, errors.New(fmt.Sprintf("Unable to parse log entry beginning with '%s'", beforeColon))
 }
 
 // ParseLogRotationLogEntry parses a log line that we already know
@@ -80,6 +104,30 @@ func ParseLogRotationLogEntry(timestamp time.Time, remainder string) (LogEntry, 
 	log.WithFields(log.Fields{
 		"timestamp":         ent.T,
 		"rotation_interval": ent.RotationInterval,
+	}).Debug("Parsed log entry")
+	return ent, nil
+}
+
+// ParseLogVersionLogEntry parses a log line that we already know
+// indicates the logfile version.
+func ParseLogVersionLogEntry(timestamp time.Time, remainder string) (LogEntry, error) {
+	var re *regexp.Regexp
+	var ent *LogVersionLogEntry
+	var groups []string
+
+	re = regexp.MustCompile(`LOG VERSION: (.*)`)
+	groups = re.FindStringSubmatch(remainder)
+	if groups == nil {
+		return nil, errors.New(fmt.Sprintf("Unable to parse version for log line '%s'", remainder))
+	}
+
+	ent = &LogVersionLogEntry{
+		T:       timestamp,
+		Version: groups[1],
+	}
+	log.WithFields(log.Fields{
+		"timestamp":   ent.T,
+		"log_version": ent.Version,
 	}).Debug("Parsed log entry")
 	return ent, nil
 }
--- log_entry_test.go
+++ log_entry_test.go
@@ -35,27 +35,32 @@ func TestParseLog_LogRotation(t *testing.T) {
 	}
 }
 
-func DontTestParseLog_LogVersion(t *testing.T) {
+func TestParseLog_LogVersion(t *testing.T) {
 	t.Parallel()
 	var err error
 	var ent LogEntry
 	var expTime time.Time
-	var expType string
+	var expType, expStrVal string
 
 	ent, err = ParseLogLine("[1438041600] LOG VERSION: 2.0")
 	if err != nil {
 		t.Log("Got error when parsing log line:", err)
-		t.Fail()
+		t.FailNow()
 	}
 
 	expTime = time.Unix(1438041600, 0)
-	expType = "rotation"
+	expType = "version"
+	expStrVal = "2.0"
 	if !ent.Timestamp().Equal(expTime) {
 		t.Log("Wrong timestamp on log version entry. Expected", expTime, "but got", ent.Timestamp())
 		t.Fail()
 	}
 	if ent.Type() != expType {
 		t.Log("Wrong type on log version entry. Expected", expType, "but got", ent.Type())
+		t.Fail()
+	}
+	if ent.StrVal() != expStrVal {
+		t.Log("Wrong version on log version entry. Expected", expStrVal, "but got", ent.StrVal())
 		t.Fail()
 	}
 }
